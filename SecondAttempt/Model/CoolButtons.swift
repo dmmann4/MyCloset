5e5e3061473bb17bfa9397285aea5cfc653442d9
dmmann4 <43962371+dmmann4@users.noreply.github.com>
2019-09-09 10:56:48 -0400

    Add files via upload

---
+++ SecondAttempt/Model/CoolButtons.swift
@@ -0,0 +1,33 @@
+//
+//  CoolButtons.swift
+//  SecondAttempt
+//
+//  Created by David Mann on 7/14/19.
+//  Copyright © 2019 David Mann. All rights reserved.
+//
+
+//import UIKit
+//@IBDesignable
+//class CoolButtons: UIButton {
+//
+//    @IBInspectable var cornerRadius : CGFloat = 0 {
+//        didSet {
+//            layer.cornerRadius = 25
+//            layer.masksToBounds = true
+//
+//        }
+//    }
+//
+//    @IBInspectable var boarderWidth : CGFloat = 0 {
+//        didSet {
+//            layer.borderWidth = boarderWidth
+//        }
+//    }
+//
+//    @IBInspectable var boarderColor : UIColor = UIColor.black{
+//        didSet {
+//            layer.borderColor = boarderColor.cgColor
+//        }
+//    }
+//
+//}
